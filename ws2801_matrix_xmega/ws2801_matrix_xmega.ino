47b8d6c63834693321ecab5d2275d9ce5a44108a
Eero af Heurlin <eero.afheurlin@iki.fi>
2013-10-01 21:13:15 +0300

    stubity stub

--- ws2801_matrix_xmega/ws2801_matrix_xmega.ino
+++ ws2801_matrix_xmega/ws2801_matrix_xmega.ino
@@ -1,3 +1,29 @@
 /**
- * foo
+ * http://pastebin.com/BLqkE7CS <- some guy reported this worked for him for event based SPI
  */
+
+// Define the matrix configuration.
+#define ROWS (1)
+#define COLS (31)
+
+const uint16_t numleds = ROWS * COLS;
+const uint16_t numchannels = ROWS * COLS * 3;
+byte framebuffer[numchannels];
+
+void setup()
+{
+}
+
+void loop()
+{
+    // Second module to RED
+    framebuffer[3] = 0xff;
+    // Third GREEN
+    framebuffer[7] = 0xff;
+    
+    // TODO: trigger the transfer somehow
+
+    // Hang forever
+    while(true);
+}
+
