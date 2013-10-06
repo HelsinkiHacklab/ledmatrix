8dc8a39ac2fc59404af71a10271240c87350c11e
Eero af Heurlin <eero.afheurlin@iki.fi>
2013-10-06 15:50:29 +0300

    less debug

--- imageconverter/imageconverter.py
+++ imageconverter/imageconverter.py
@@ -11,13 +11,13 @@ class imageconverter:
         self.arr1 = np.asarray(self.im)
         self.arr2 = np.ndarray(shape=(7,31,3), dtype=np.uint8)
         # Flip every other row
-        print  "Before:" , repr(self.arr1)
+        #print  "Before:" , repr(self.arr1)
         for i in range(len(self.arr1)):
             if ((i % 2) == 0):
                 self.arr2[i] = self.arr1[i][::-1]
             else:
                 self.arr2[i] = self.arr1[i]
-        print  "After:" , repr(self.arr2)
+        #print  "After:" , repr(self.arr2)
         #self.im = Image.fromarray(self.arr2)
         #self.im.show()
         self.bytestream = self.arr2.flatten()
@@ -27,7 +27,7 @@ if __name__ == '__main__':
     if len(sys.argv) < 2:
         print "usage ./imageconverter.py imagefile"
     c = imageconverter(sys.argv[1])
-    im = Image.fromarray(c.arr2)
-    im.show()
+    #im = Image.fromarray(c.arr2)
+    #im.show()
     
     
