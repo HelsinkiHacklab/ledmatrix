64d5138aefd987dd1e83a7e7fbac18d18c458218
Eero af Heurlin <eero.afheurlin@iki.fi>
2013-10-06 11:37:00 +0300

    this should work, but the row flip looks a bit wrong

--- imageconverter/imageconverter.py
+++ imageconverter/imageconverter.py
@@ -7,14 +7,24 @@ import numpy as np
 class imageconverter:
     def __init__(self, imagedata):
         self.im = Image.open(imagedata).convert('RGB')
-        self.arr = np.asarray(self.im)
-        
-
+        #self.im.show()
+        self.arr = np.copy(np.asarray(self.im))
+        # Flip every other row
+        print  "Before:" , repr(self.arr)
+        for i in range(len(self.arr)):
+            if ((i % 2) == 0):
+                # no, too smart for it's own good
+                #self.arr[i] = np.fliplr(self.arr[i])
+                self.arr[i] = self.arr[i][::-1]
+        print  "After:" , repr(self.arr)
+        #self.im = Image.fromarray(self.arr)
+        #self.im.show()
+        self.bytestream = self.arr.flatten()
 
 # Another small bit of boilerplate
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print "usage ./imageconverter.py imagefile"
     c = imageconverter(sys.argv[1])
-    print c.arr
+    
     
