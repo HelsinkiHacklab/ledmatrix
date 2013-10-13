56f33f80abd5ba1c3eeeb73caa9f8c17c895a0b2
Eero af Heurlin <eero.afheurlin@iki.fi>
2013-10-13 20:36:44 +0300

    trivial application to test the bridge

---
+++ spidev_zmq/echotest.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+import sys,os,time
+import zmq
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print "usage ./echotest.py tcp://whatever:6969"
+    data = [ chr(0xde), chr(0xad), chr(0xbe), chr(0xef) ]
+
+    context = zmq.Context()
+    socket = self.context.socket(zmq.REQ)
+    socket.bind(sys.argv[1])
+    
+    socket.send(data)
+    print "Sent %s" % repr(data)
+    rpl = socket.recv()
+    print "Got reply %s" % repr(rpl)
