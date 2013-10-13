7ebd85f70d1c8a1ecda2dbd6106f210428e75c00
Eero af Heurlin <eero.afheurlin@iki.fi>
2013-10-13 23:23:52 +0300

    test increasing size of payload

--- spidev_zmq/echotest.py
+++ spidev_zmq/echotest.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python
 import sys,os,time
 import zmq
+import random
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print "usage ./echotest.py tcp://whatever:6969"
-    data = [ chr(0xde), chr(0xad), chr(0xbe), chr(0xef) ]
-
+    if len(sys.argv) < 3:
+        print "usage ./echotest.py tcp://whatever:6969 uptobytes"
+        
     context = zmq.Context()
     socket = context.socket(zmq.REQ)
     socket.connect(sys.argv[1])
-    
-    socket.send(''.join(data))
-    print "Sent %s" % repr(data)
-    rpl = socket.recv()
-    print "Got reply %s" % repr(rpl)
+
+    for x in range(1, int(sys.argv[2]
+        data = [ chr(random.randint(0,255)) for xx in range(1,x) ]
+        datastr = ''.join(data)
+        socket.send()
+        print "Sent %s" % repr(datastr)
+        rpl = socket.recv()
+        print "Got reply %s" % repr(rpl)
