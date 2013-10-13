f565f14b63c1743fd5940b27d0ff1ecb88d01054
Eero af Heurlin <eero.afheurlin@iki.fi>
2013-10-13 16:47:23 +0300

    merge the SPI code

--- spidev_zmq/spidev_zmq.c
+++ spidev_zmq/spidev_zmq.c
@@ -1,15 +1,27 @@
 /** 
  * Trivial REPly handler copied from ZMQ example: https://raw.github.com/imatix/zguide/master/examples/C/interrupt.c
  *
- * TODO: from https://www.kernel.org/doc/Documentation/spi/spidev_test.c copy the SPI interface
+ * And SPI handling from https://www.kernel.org/doc/Documentation/spi/spidev_test.c 
+ *
+ * LGPLv2
  */
 
-
-//  Shows how to handle Ctrl-C
-
+// ZMQ includes
 #include <zmq.h>
 #include <stdio.h>
 #include <signal.h>
+
+// spidev includes
+#include <stdint.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <linux/types.h>
+#include <linux/spi/spidev.h>
+
 
 //  Signal handling
 //
@@ -33,11 +45,169 @@ static void s_catch_signals (void)
     sigaction (SIGTERM, &action, NULL);
 }
 
+#define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
+
+static void pabort(const char *s)
+{
+    perror(s);
+    abort();
+}
+
+// The SPI configs
+static const char *device = "/dev/spidev1.1";
+static uint8_t mode;
+static uint8_t bits = 8;
+static uint32_t speed = 500000;
+static uint16_t delay;
+// ZMQ cofig
+static const char *zmq_socket = "tpc://*:6969";
+
+static void print_usage(const char *prog)
+{
+    printf("Usage: %s [-DsbdlHOLC3NRS]\n", prog);
+    puts("  -D --device   device to use (default /dev/spidev1.1)\n"
+         "  -s --speed    max speed (Hz)\n"
+         "  -d --delay    delay (usec)\n"
+         "  -b --bpw      bits per word \n"
+         "  -l --loop     loopback\n"
+         "  -H --cpha     clock phase\n"
+         "  -O --cpol     clock polarity\n"
+         "  -L --lsb      least significant bit first\n"
+         "  -C --cs-high  chip select active high\n"
+         "  -3 --3wire    SI/SO signals shared\n"
+         "  -N --no-cs    set SPI_NO_CS\n"
+         "  -R --ready    set SPI_READY\n"
+         "  -S --socket   ZMQ socket definition\n"
+    );
+    exit(1);
+}
+
+static void parse_opts(int argc, char *argv[])
+{
+    while (1) {
+        static const struct option lopts[] = {
+            { "device",  1, 0, 'D' },
+            { "speed",   1, 0, 's' },
+            { "delay",   1, 0, 'd' },
+            { "bpw",     1, 0, 'b' },
+            { "loop",    0, 0, 'l' },
+            { "cpha",    0, 0, 'H' },
+            { "cpol",    0, 0, 'O' },
+            { "lsb",     0, 0, 'L' },
+            { "cs-high", 0, 0, 'C' },
+            { "3wire",   0, 0, '3' },
+            { "no-cs",   0, 0, 'N' },
+            { "ready",   0, 0, 'R' },
+            { "socket",  0, 0, 'S' },
+            { NULL, 0, 0, 0 },
+        };
+        int c;
+
+        c = getopt_long(argc, argv, "D:s:d:b:lHOLC3NRS", lopts, NULL);
+
+        if (c == -1)
+            break;
+
+        switch (c) {
+        case 'D':
+            device = optarg;
+            break;
+        case 'S':
+            zmq_socket = optarg;
+            break;
+        case 's':
+            speed = atoi(optarg);
+            break;
+        case 'd':
+            delay = atoi(optarg);
+            break;
+        case 'b':
+            bits = atoi(optarg);
+            break;
+        case 'l':
+            mode |= SPI_LOOP;
+            break;
+        case 'H':
+            mode |= SPI_CPHA;
+            break;
+        case 'O':
+            mode |= SPI_CPOL;
+            break;
+        case 'L':
+            mode |= SPI_LSB_FIRST;
+            break;
+        case 'C':
+            mode |= SPI_CS_HIGH;
+            break;
+        case '3':
+            mode |= SPI_3WIRE;
+            break;
+        case 'N':
+            mode |= SPI_NO_CS;
+            break;
+        case 'R':
+            mode |= SPI_READY;
+            break;
+        default:
+            print_usage(argv[0]);
+            break;
+        }
+    }
+}
+
+
 int main (void)
 {
+	int ret = 0;
+	int spidev_fd;
+
+	parse_opts(argc, argv);
+
+	spidev_fd = open(device, O_RDWR);
+	if (spidev_fd < 0)
+		pabort("can't open device");
+
+	/*
+	 * spi mode
+	 */
+	ret = ioctl(spidev_fd, SPI_IOC_WR_MODE, &mode);
+	if (ret == -1)
+		pabort("can't set spi mode");
+
+	ret = ioctl(spidev_fd, SPI_IOC_RD_MODE, &mode);
+	if (ret == -1)
+		pabort("can't get spi mode");
+
+	/*
+	 * bits per word
+	 */
+	ret = ioctl(spidev_fd, SPI_IOC_WR_BITS_PER_WORD, &bits);
+	if (ret == -1)
+		pabort("can't set bits per word");
+
+	ret = ioctl(spidev_fd, SPI_IOC_RD_BITS_PER_WORD, &bits);
+	if (ret == -1)
+		pabort("can't get bits per word");
+
+	/*
+	 * max speed hz
+	 */
+	ret = ioctl(spidev_fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed);
+	if (ret == -1)
+		pabort("can't set max speed hz");
+
+	ret = ioctl(spidev_fd, SPI_IOC_RD_MAX_SPEED_HZ, &speed);
+	if (ret == -1)
+		pabort("can't get max speed hz");
+
+	printf("spi mode: %d\n", mode);
+	printf("bits per word: %d\n", bits);
+	printf("max speed: %d Hz (%d KHz)\n", speed, speed/1000);
+
+
     void *context = zmq_ctx_new ();
     void *socket = zmq_socket (context, ZMQ_REP);
-    zmq_bind (socket, "tcp://*:5555");
+    zmq_bind (socket, zmq_socket);
 
     s_catch_signals ();
     while (1) {
@@ -51,5 +221,7 @@ int main (void)
     }
     zmq_close (socket);
     zmq_ctx_destroy (context);
+
+	close(spidev_fd);
     return 0;
 }
