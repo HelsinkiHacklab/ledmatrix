ec13c2bf57b3c6076ac2c2efc8ca2bfad0bc2ad3
Eero af Heurlin <eero.afheurlin@iki.fi>
2013-10-14 11:17:09 +0300

    minor fixes

--- spidev_zmq/echotest.py
+++ spidev_zmq/echotest.py
@@ -6,19 +6,23 @@ import random
 if __name__ == '__main__':
     if len(sys.argv) < 3:
         print "usage ./echotest.py tcp://whatever:6969 uptobytes [frombytes]"
+        sys.exit(1)
         
     context = zmq.Context()
     socket = context.socket(zmq.REQ)
     socket.connect(sys.argv[1])
 
+    tobytes = int(sys.argv[2])
     frombytes = 1
     if len(sys.argv) >= 4:
         frombytes = int(sys.argv[3])
 
-    for x in range(frombytes, int(sys.argv[2])):
+    print "Iterating from %d to %d" % (frombytes, tobytes)
+    for x in range(frombytes, tobytes):
+        print "Iteration #%d" % x
         data = [ chr(random.randint(0,255)) for xx in range(1,x) ]
         datastr = ''.join(data)
         socket.send(datastr)
         print "Sent %s" % repr(datastr)
         rpl = socket.recv()
-        print "Got reply %s" % repr(rpl)
+        print "Got reply (%d bytes): %s" % (len(rpl), repr(rpl))
