4625cfe2646627577908bb73685abf532fa1f2c9
Eero af Heurlin <rambo@iki.fi>
2013-11-04 22:11:49 +0200

    this works

--- pyaudio_tests/pyqt_pil_test.py
+++ pyaudio_tests/pyqt_pil_test.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 import sys
 from PyQt4 import QtCore, QtGui
-import Image
+#import Image
 import ImageQt
+from PIL import Image, ImageOps
 
 
 class MyWidget(QtGui.QWidget):
@@ -13,18 +14,27 @@ class MyWidget(QtGui.QWidget):
         self.setWindowTitle('My Widget!')
 
 
-        PilImage = Image.open('kitten1.jpg')
-        QtImage1 = ImageQt.ImageQt(PilImage)
+        self.label = QtGui.QLabel('', self)
+        self.PilImage = Image.open('kitten1.jpg')
+        self.update_image()
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.invert)
+        self.timer.start(2500)
+
+    def update_image(self):
+        QtImage1 = ImageQt.ImageQt(self.PilImage)
         QtImage2 = QtImage1.copy()
         pixmap = QtGui.QPixmap.fromImage(QtImage2)
-        label = QtGui.QLabel('', self)
-        label.setPixmap(pixmap)
+        self.label.setPixmap(pixmap)
+
+    def invert(self):
+        self.PilImage = ImageOps.invert(self.PilImage)
+        self.update_image()
 
 
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
     myWidget = MyWidget()
     myWidget.show()
-
     sys.exit(app.exec_())
 
