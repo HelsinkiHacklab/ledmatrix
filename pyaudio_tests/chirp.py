4111e7d368980ec02f2dcab4c51a349621ba5bef
Eero af Heurlin <eero.afheurlin@iki.fi>
2013-11-06 01:24:42 +0200

    typo fix

--- pyaudio_tests/chirp.py
+++ pyaudio_tests/chirp.py
@@ -15,7 +15,7 @@ chirp_start_hz = 20.0
 chirp_end_hz = 1500.0
 chirp_time_s = 3
 
-# TODO: Probably should do multiple consequtive sweeps instead of calculating all this into memory in one go...
+# TODO: Probably should do multiple consecutive sweeps instead of calculating all this into memory in one go...
 sampletimes = numpy.linspace(0.0, chirp_time_s, chirp_time_s*sampleRate)
 chirpdata = scipy.signal.chirp(sampletimes, chirp_start_hz, chirp_time_s, chirp_end_hz)
 audio_chirp = numpy.interp(chirpdata, [-1.0, 1.0], [0, 2**16-1]).astype(numpy.uint16)
