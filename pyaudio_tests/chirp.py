9d55369db73098a1be6c2fe0e8b09e5183942f77
Eero af Heurlin <eero.afheurlin@iki.fi>
2013-11-06 01:35:13 +0200

    adjusting the buffersize does not really help

--- pyaudio_tests/chirp.py
+++ pyaudio_tests/chirp.py
@@ -7,12 +7,13 @@ import scipy.signal
 sampleRate=44100
 #sampleRate=1000
 bufferSize=2**10 # 1024
+buffersize=512
 
 p = pyaudio.PyAudio()
 stream = p.open(format=pyaudio.paInt16, channels=1, rate=sampleRate, input=False, output=True, frames_per_buffer=bufferSize)
 
-chirp_start_hz = 20.0
-chirp_end_hz = 1500.0
+chirp_start_hz = 50.0
+chirp_end_hz = 7000.0
 chirp_time_s = 10
 
 # TODO: Probably should do multiple consecutive sweeps instead of calculating all this into memory in one go...
